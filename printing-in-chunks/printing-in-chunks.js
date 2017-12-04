a64b53a589bf0ab76525d221723f0932c426e026
ItsJustChewbacca <itsjustchewbacca@gmail.com>
2017-12-04 20:04:41 +0000

    printing html chunks

---
+++ printing-in-chunks/printing-in-chunks.js
@@ -0,0 +1,17 @@
+function getAndPrintHTMLChunks() {
+  var requestOptions = {
+    host: "sytantris.github.io",
+    path: "/http-examples/step1.html"
+  };
+  var https = require("https");
+  https.get(requestOptions, function(response) {
+    response.setEncoding("utf8");
+    response.on("data", function(data) {
+      console.log(data + "/n");
+    });
+    response.on("end", function() {
+      console.log("response stream complete.");
+    });
+  });
+}
+getAndPrintHTMLChunks();
